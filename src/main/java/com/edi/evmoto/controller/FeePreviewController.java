8b34eb646601eb0274b4c0f0a72fd3a4d7dd986b
ediMariyanto <edimariyanto491@gmail.com>
2026-08-12 16:06:33 +0700

    feat: add POST method in FeePreviewController

--- src/main/java/com/edi/evmoto/controller/FeePreviewController.java
+++ src/main/java/com/edi/evmoto/controller/FeePreviewController.java
@@ -1,12 +1,18 @@
 package com.edi.evmoto.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.edi.evmoto.dto.FeePreviewRequest;
+import com.edi.evmoto.dto.FeePreviewResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/orders")
 public class FeePreviewController {
 
-
+    @PostMapping("/{orderId}/fee-preview")
+    public ResponseEntity<FeePreviewResponse> feePreview(@PathVariable("orderId") String orderId, @RequestBody FeePreviewRequest request){
+        FeePreviewResponse response = null;
+        return ResponseEntity.ok(response);
+    }
 
 }
