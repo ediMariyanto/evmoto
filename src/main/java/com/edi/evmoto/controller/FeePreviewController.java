9eeb992aa3cce2af6083da66b4926d7691eb0bb5
ediMariyanto <edimariyanto491@gmail.com>
2026-08-12 16:16:32 +0700

    refactor: FeePreviewController, linked to FeePreviewService service

--- src/main/java/com/edi/evmoto/controller/FeePreviewController.java
+++ src/main/java/com/edi/evmoto/controller/FeePreviewController.java
@@ -2,6 +2,8 @@ package com.edi.evmoto.controller;
 
 import com.edi.evmoto.dto.FeePreviewRequest;
 import com.edi.evmoto.dto.FeePreviewResponse;
+import com.edi.evmoto.service.FeePreviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.*;
 public class FeePreviewController {
 
 
+    @Autowired
+    FeePreviewService feePreviewService;
 
     @PostMapping("/{orderId}/fee-preview")
     public ResponseEntity<FeePreviewResponse> feePreview(@PathVariable("orderId") String orderId, @RequestBody FeePreviewRequest request){
-        FeePreviewResponse response = null;
+        FeePreviewResponse response = feePreviewService.feePreview(orderId, request);
         return ResponseEntity.ok(response);
     }
 
