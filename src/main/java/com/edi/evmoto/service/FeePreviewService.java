177b15126e09e6e0124366e2a2d8bdb30a219e35
ediMariyanto <edimariyanto491@gmail.com>
2026-08-12 16:15:39 +0700

    refactor: FeePreviewService, linked to WaitingFeeCalculator service

--- src/main/java/com/edi/evmoto/service/FeePreviewService.java
+++ src/main/java/com/edi/evmoto/service/FeePreviewService.java
@@ -1,7 +1,17 @@
 package com.edi.evmoto.service;
 
+import com.edi.evmoto.dto.FeePreviewRequest;
+import com.edi.evmoto.dto.FeePreviewResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FeePreviewService {
+
+    @Autowired
+    WaitingFeeCalculatorService waitingFeeCalculatorService;
+
+    public FeePreviewResponse feePreview(String orderId,FeePreviewRequest request) {
+        return waitingFeeCalculatorService.calculate(orderId, request);
+    }
 }
